b35f91c1cc43bef1eceb61026700b3289e8fe209
olst0004 <olst0004@stud.hs-kl.de>
2020-05-05 10:42:15 +0200

    Initial Upload
    Unity-Project added to repo, gitignore updated

---
+++ PointCloudViz/Assets/04_Animation/AnimationControls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControls : MonoBehaviour
+{
+    public Animator myAnimator;
+
+    Vector2 speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        speed.x = Input.GetAxisRaw("Vertical");
+
+        myAnimator.SetFloat("Speed", speed.x);
+        myAnimator.SetFloat("speedNormalized", Mathf.Abs(speed.x));
+    }
+}
